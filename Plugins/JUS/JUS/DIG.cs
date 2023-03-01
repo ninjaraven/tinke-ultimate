49ef082a2a5371234c25169fbb8897deab41f786
The Best NSMB <58012953+TheBestNSMB@users.noreply.github.com>
2023-02-28 22:23:05 -0500

    upgrade JUS project to .NET 4.8

--- Plugins/JUS/JUS/DIG.cs
+++ Plugins/JUS/JUS/DIG.cs
@@ -1,5 +1,6 @@
 // ----------------------------------------------------------------------
 // <copyright file="DIG.cs" company="none">
+
 // Copyright (C) 2019
 //
 //   This program is free software: you can redistribute it and/or modify
@@ -16,6 +17,7 @@
 //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // </copyright>
+
 // <author>Priverop</author>
 // <contact>https://github.com/priverop/</contact>
 // <date>25/02/2019</date>
@@ -25,88 +27,67 @@ using System.IO;
 using System.Drawing;
 using Ekona;
 using Ekona.Images;
+
 namespace JUS
 {
     class DIG : ImageBase
     {
         IPluginHost pluginHost;
+
         public DIG(IPluginHost pluginHost, string file, int id, string fileName = "")
         {
             this.pluginHost = pluginHost;
             this.id = id;
             this.fileName = fileName;
+
             Read(file);
         }
+
         public override void Read(string fileIn)
         {
             byte[] data = File.ReadAllBytes(fileIn);
 
-            byte paletteType = data[5];
-            byte paletteSize = data[6];
             byte formatType = data[5];
             byte numPaletteLines = data[6];
 
             short width = BitConverter.ToInt16(data, 8);
             short height = BitConverter.ToInt16(data, 10);
 
-            int paletteEnd = paletteSize * 32 + 12;
             int paletteEnd = numPaletteLines * 32 + 12;
 
             int startPalette = 12;
             int position = startPalette;
 
-            if (BitConverter.ToInt32(data, position) == 0)
-            {
-                position += 4;
-                while (BitConverter.ToInt32(data, position) == 0) { position += 4; }
-                startPalette = position;
-            }
-
-            int paletteActualSize = paletteEnd - startPalette;
             int paletteSize = paletteEnd - startPalette;
 
-            position = startPalette;
             ColorFormat format;
-            RawPalette palette;
-
-            if (paletteType == 16)
             RawPalette palette;
             Color[][] palettes;
 
             if ((formatType & 0x0F) == 0)
             {
-                int paletteColors = 16;
-                int paletteColorSize = paletteColors * 2;
                 format = ColorFormat.colors16;
-                Color[][] palettes;
-                decimal paletteNumber = Math.Ceiling((decimal)paletteActualSize / paletteColorSize);
-
-                palettes = new Color[(int)paletteNumber][];
                 palettes = new Color[numPaletteLines][];
 
-                for (int i = 0; i < paletteNumber; i++)
                 for (int i = 0; i < numPaletteLines; i++)
                 {
-                    byte[] aux = new byte[paletteColorSize];
-                    Array.Copy(data, position, aux, 0, paletteColorSize);
                     byte[] aux = new byte[0x20];
                     Array.Copy(data, startPalette + (i * 0x20), aux, 0, 0x20);
 
                     palettes[i] = Actions.BGR555ToColor(aux);
                 }
-@@ -94,20 +82,26 @@ public override void Read(string fileIn)
+
+                palette = new RawPalette(palettes, false, format);
+            }
             else
             {
                 format = ColorFormat.colors256;
-                byte[] aux = new byte[paletteActualSize];
-                Array.Copy(data, position, aux, 0, paletteActualSize);
-                Color[] p = Actions.BGR555ToColor(aux);
-                palette = new RawPalette(new Color[][] { p }, false, format);
                 int numPalettes = ((numPaletteLines - 1) / 16) + 1;
 
                 palettes = new Color[numPalettes][];
 
-                for(int i = 0; i < numPalettes; i++) {
+                for (int i = 0; i < numPalettes; i++)
+                {
                     byte[] aux = new byte[0x200];
                     Array.Copy(data, startPalette + (i * 0x200), aux, 0, 0x200);
                     palettes[i] = Actions.BGR555ToColor(aux);
@@ -115,20 +96,17 @@ namespace JUS
                 palette = new RawPalette(palettes, false, format);
 
             }
-            position = paletteEnd;
 
             pluginHost.Set_Palette(palette);
 
             // Get image
-            byte[] tiles = new byte[data.Length - position];
-            Array.Copy(data, position, tiles, 0, data.Length - position);
-            Set_Tiles(tiles, width, height, format, TileForm.Horizontal, false);
             byte[] tiles = new byte[data.Length - paletteEnd];
             Array.Copy(data, paletteEnd, tiles, 0, data.Length - paletteEnd);
             Set_Tiles(tiles, width, height, format, formatType >> 4 == 1 ? TileForm.Horizontal : TileForm.Lineal, false);
 
             pluginHost.Set_Image(this);
         }
+
         public override void Write(string fileOut, PaletteBase palette)
         {
         }
